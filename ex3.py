3b3d938a4be4ad4c8f9ca50c11e06143fc3ea635
Pranav <31311095+Pranav222@users.noreply.github.com>
2017-09-28 12:06:08 +0530

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -6,20 +6,20 @@ print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
 # print the below strings
 print("Now I will count the eggs:")
 #arithmetic operations
-print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
-#Cheking the staments is True or False
-print("Is it true that 3 + 2 < 5 - 7?")
-#Cheking the staments is True or False based on the result of arithmetic operation
-print(3 + 2 < 5 - 7) # This is a false statement
-#print the result of addition 
-print("What is 3 + 2?", 3 + 2)
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+#Print the below strings
+print("Is it true that 3.0 + 2.0 < 5.0 - 7.0?")
+#Cheking the staments is Truth or False
+print(3.0 + 2.0 < 5.0 - 7.0) # This is a false statement
+#print the result of addition
+print("What is 3 + 2?", 3.0 + 2.0)
 #print the result of subtraction
-print("What is 5 - 7?", 5 - 7)
+print("What is 5.0 - 7.0?", 5.0 - 7.0)
 #The above statement is False. So print the below strings.
 print("Oh, that's why it's False.")
 #this will print the below string
 print("How about some more.")
 #Cheking the conditions are Truth or False
-print("Is it greater?", 5 > -2)
-print("Is it greater or equal?", 5 >= -2)
-print("Is it less or equal?", 5 <= -2)
+print("Is it greater?", 5.0 > -2.0)
+print("Is it greater or equal?", 5.0 >= -2.0)
+print("Is it less or equal?", 5.0 <= -2.0)
