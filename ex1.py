2c7fff2ca041b6a005fc57e5044c80165aa960a9
pranav <pranavkv222@gmail.com>
2017-09-28 05:41:51 +0000

    LP3THW_ex1

---
+++ ex1.py
@@ -0,0 +1,10 @@
+# octothorpe character '#'
+#print the given strings
+print("Hello World!\n")
+print("Hello Again")
+print("I like typing this.")
+# Anything after the # is ignored by python.
+#print("This is fun.")
+#print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
