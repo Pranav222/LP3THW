d98e01e805734fdc6f3de0ccf70e25f191c89108
pranav <pranavkv222@gmail.com>
2017-09-28 06:17:55 +0000

    LP3THW_ex3.py

---
+++ ex3.py
@@ -0,0 +1,25 @@
+# this will print the below string
+print("I will now count my chickens:")
+# finding total number of Hens and Roosters
+print("Hens", 25.0 + 30.0 / 6.0)
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+# print the below strings
+print("Now I will count the eggs:")
+#arithmetic operations
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+#Cheking the staments is True or False
+print("Is it true that 3 + 2 < 5 - 7?")
+#Cheking the staments is True or False based on the result of arithmetic operation
+print(3 + 2 < 5 - 7) # This is a false statement
+#print the result of addition 
+print("What is 3 + 2?", 3 + 2)
+#print the result of subtraction
+print("What is 5 - 7?", 5 - 7)
+#The above statement is False. So print the below strings.
+print("Oh, that's why it's False.")
+#this will print the below string
+print("How about some more.")
+#Cheking the conditions are Truth or False
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
