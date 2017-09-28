c4654e111bf0cec9c6b364a669758527e0a3b1a1
pranav <pranavkv222@gmail.com>
2017-09-28 10:38:25 +0000

    LP3THW_ex9

---
+++ ex9.py
@@ -0,0 +1,20 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+print("Here are the days: ",days)
+print("Here are the months : ",months)
+
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
+
+print("""
+I Never played with a runner in
+my entire life, even in schools!
+Bcs only i know where the ball is
+going and how hard.When I hit the
+ball,something my runner will never
+know about.
+""")
