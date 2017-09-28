9253daffd5fe1bbd14a33949a11ca2e79973e4c2
pranav <pranavkv222@gmail.com>
2017-09-28 07:53:16 +0000

    LP3THW_ex5

---
+++ ex5.py
@@ -0,0 +1,24 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+# study drill
+inches_to_centimeter = height * 2.54
+pound_to_Kg = weight * 0.45359237
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
+
+#study drill
+print(f"converted inches to centimeter = {inches_to_centimeter}")
+print(f"Convered pound to kilogram {pound_to_Kg}")
