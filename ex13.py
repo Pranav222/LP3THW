8648f5b5d3327493d5f5866e15fdc228a0828183
pranav <pranavkv222@gmail.com>
2017-09-29 05:53:43 +0000

    LP3THW_ex13

---
+++ ex13.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+file_name, name, age, weight = argv
+
+print("The script is called:", file_name)
+print("Your name is:", name)
+print("Your age is:", age)
+print("Your weight is:", weight)
+
+x =input("Enter your name : ")
+y= input("Enter your age : ")
+z= input("Enter your weight :")
